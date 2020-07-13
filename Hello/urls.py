29b0f087b5573ec53ff280d796fe9cf6d2344910
kishan9192 <mishra.kishan1@gmail.com>
2020-07-13 12:01:41 +0530

    commit on master

--- Hello/urls.py
+++ Hello/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Kishan's Database"
+admin.site.site_title = "Database"
+admin.site.index_title = "Welcome to the database"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
